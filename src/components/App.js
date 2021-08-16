8aeb91154052135853849810336bfd6e3b03bd2f
Dominic DeRosa <dderosa71@gmail.com>
2021-08-16 19:35:56 +0200

    commit tree 8eb699d366b8f3af51ea1df25fa8c345e7d138ea

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { useState, useEffect } from "react"
+
+
+function App() {
+    const [loading, setLoading] = useState(true)
+    const [images, setImages] = useState("")
+
+    useEffect(() => fetch('https://dog.ceo/api/breeds/image/random')
+        .then(resp => resp.json())
+        .then(json => {
+            console.log(json)
+            setLoading(false)
+            setImages(json.message)
+        }),[]
+
+    )
+
+    if (loading){
+    return (
+        <p>Loading...</p>
+    )
+    }
+    else{return (
+        <img src={images} alt="A Random Dog"/>
+        )
+    }
+}
+
+export default App
